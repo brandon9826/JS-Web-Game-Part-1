d095bdc95269d6558459335f37b7d3d21401f8c9
brandon9826 <brandonl2426@gmail.com>
2022-03-23 17:12:32 -0500

    second

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function newImage (imgSrc, fixedLeft, fixedBottom, itemFalsey) {
         })
     }
 }
-//Images
+//function calls//-----------------------------------------
 newImage('assets/green-character.gif', '100px','100px',false)
 newImage('assets/pine-tree.png', '450px','200px',false)
 newImage("assets/tree.png", "200px", "300px",false)
 newImage("assets/pillar.png", "350px", "100px",false)
 newImage("assets/crate.png", "150px", "200px",false)
 newImage("assets/well.png","500px", "425px",false)
-//Clickable Images
+//Clickable Images //----------------------------
 newImage("assets/sword.png", "500px", "405px",true)
 newImage("assets/shield.png", "165px", "185px",true)
 newImage("assets/staff.png", "600px", "100px", true)
