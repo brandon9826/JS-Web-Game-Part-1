91bc55f285d9d91f1a9980ed43127e81a6050428
brandon9826 <brandonl2426@gmail.com>
2022-03-23 17:08:41 -0500

    first

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (imgSrc, fixedLeft, fixedBottom, itemFalsey) {
+    let item = document.createElement('img')
+    item.src = imgSrc
+    item.style.position = 'fixed'
+    item.style.left = fixedLeft
+    item.style.bottom = fixedBottom
+    document.body.append(item)
+    if(itemFalsey){
+        item.addEventListener("dblclick", function () {
+            item.remove()
+        })
+    }
+}
+//Images
+newImage('assets/green-character.gif', '100px','100px',false)
+newImage('assets/pine-tree.png', '450px','200px',false)
+newImage("assets/tree.png", "200px", "300px",false)
+newImage("assets/pillar.png", "350px", "100px",false)
+newImage("assets/crate.png", "150px", "200px",false)
+newImage("assets/well.png","500px", "425px",false)
+//Clickable Images
+newImage("assets/sword.png", "500px", "405px",true)
+newImage("assets/shield.png", "165px", "185px",true)
+newImage("assets/staff.png", "600px", "100px", true)
+
+
